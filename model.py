cafa3e5695442b3ac1e1b4c822581405b7a1ebc8
Oliver Hulland <olihulland@gmail.com>
2023-05-02 01:18:44 +0100

    generate tensorflow model locally using data

---
+++ model.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+with open("out.csv", "r") as file:
+    features = []
+    labels = []
+    for line in file:
+        asList = line.split(",")
+        features.append(asList[:-1])
+        labels.append(asList[-1].strip())
+
+    for i, label in enumerate(labels):
+        labels[i] = int(label)-1
+
+    features = pd.DataFrame(features).to_numpy()
+    labels = np.array(labels)
+
+    labels = labels.astype(int)
+    features = features.astype(float)
+
+    # shuffle
+    indices = np.arange(features.shape[0])
+    np.random.shuffle(indices)
+    features = features[indices]
+    labels = labels[indices]
+
+    # normalize
+    features = (features - features.mean(axis=0)) / features.std(axis=0)
+
+    # split into train and test
+    train_size = int(len(features) * 0.8)
+    test_size = len(features) - train_size
+    train_features, test_features = features[0:train_size], features[train_size:len(features)]
+    train_labels, test_labels = labels[0:train_size], labels[train_size:len(labels)]
+
+    # Define the model architecture
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(64, activation='relu', input_shape=(13,)),
+        tf.keras.layers.Dense(3, activation='sigmoid')
+    ])
+
+    # Compile the model.
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(0.001),
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy'])
+    
+    # Train the model.
+    model.fit(train_features, train_labels, epochs=40, batch_size=32)
+
+    # Evaluate the model.
+    print("Evaluating model...")
+    model.evaluate(test_features, test_labels)
